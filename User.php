951ee5259646adc2a6a397d279e4f668af73b03a
alessandro-gennaccari <alessandrogennaccari@gmail.com>
2021-03-15 15:16:15 +0100

    added class User

---
+++ User.php
@@ -0,0 +1,50 @@
+<?php
+
+class User {
+
+    public $name;
+    public $lastname;
+    public $username;
+    public $email;
+    private $password;
+    public $date;
+
+
+    public function __construct($_username, $_email){
+
+        $this->username = $_username;
+        $this->email = $_email;
+
+    }
+
+
+    public function setPassword($_password){
+
+        if (strlen($_password) < 5){
+            throw new Exception('Password poco sicura');
+        } else {
+            $this->password = $_password;
+        }
+
+    }
+
+    public function getPassword(){
+
+        return $this->password;
+
+    }
+
+    public function setDate(){
+
+        $this->date = date("F j, Y, g:i a"); 
+
+    }
+
+    public function getDate(){
+
+        return $this->date;
+
+    }
+
+
+}
